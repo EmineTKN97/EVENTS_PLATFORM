5e81e8f937a8f0cd71fffd82493471bacad2f6ac
Emine Tekin <eminetekin97@outlook.com>
2024-04-04 00:09:48 +0300

    +createDbContext +addEventService

---
+++ Infrastructure/Events_Onion.Persistance/DbContexts/EventDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Events_Onion.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Onion.Persistance.DbContexts
+{
+   public class EventDbContext:DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("EventDB");
+        }
+    }
+}
---
+++ Infrastructure/Events_Onion.Persistance/Services/EventService.cs
@@ -0,0 +1,45 @@
+﻿using Events_Onion.Application.Abstractions.Services;
+using Events_Onion.Application.DTOs;
+using Events_Onion.Application.RequestParameters;
+using Events_Onion.Domain.Entities;
+using Events_Onion.Persistance.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Onion.Persistance.Services
+{
+    public class EventService(EventDbContext _context): IEventService
+    {
+
+        public async Task CreateEventAsync(CreateEventDTO createEventDTO)
+        {
+            var newEvent = new Event()
+            {
+                Title = createEventDTO.Title,
+                Date = createEventDTO.Date,
+                Location = createEventDTO.Location,
+            };
+           await _context.Events.AddAsync(newEvent);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<EventDTO>> GetAllEventsAsync(Pagination pagination)
+        {
+            return await _context.Events.Select(x => new EventDTO()
+                { 
+                Title = x.Title,
+                Date = x.Date,
+                Location = x.Location,
+                
+            }) 
+                .Skip(pagination.PageCount *pagination.ItemCount)
+                .Take(pagination.ItemCount)
+                .ToListAsync();
+        }
+    }
+}
