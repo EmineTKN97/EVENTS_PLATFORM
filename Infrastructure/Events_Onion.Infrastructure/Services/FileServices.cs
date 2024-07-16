c2c04c226d3a513250e86c4950c0d4fc19852be7
Emine Tekin <eminetekin97@outlook.com>
2024-07-16 14:08:31 +0300

    +implemet textserviceandfileservice

---
+++ Infrastructure/Events_Onion.Infrastructure/Services/FileServices.cs
@@ -0,0 +1,29 @@
+﻿using Events_Onion.Application.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Onion.Infrastructure.Services
+{
+	public class FileServices : IFileService
+	{
+		public async Task SaveTextAsync(string text, string path)
+		{
+			try
+			{
+				if (string.IsNullOrWhiteSpace(path))
+				{
+					throw new ArgumentNullException(nameof(path));
+				}
+
+				await File.WriteAllTextAsync(text, path);
+			}
+			catch (Exception ex) 
+			{
+				Console.WriteLine($"An error occured while saving the text to file :{ex.Message}");
+			}
+		}
+	}
+}
---
+++ Infrastructure/Events_Onion.Infrastructure/Services/TextService.cs
@@ -0,0 +1,26 @@
+﻿using Events_Onion.Application.Abstractions.Services;
+using Events_Onion.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Onion.Infrastructure.Services
+{
+	public class TextService : ITextService
+	{
+		public string FormatTextForEvent(IEnumerable<EventDTO> eventItems)
+		{
+			if (eventItems is null)
+				throw new ArgumentNullException(nameof(eventItems));
+
+			StringBuilder stringBuilder = new StringBuilder();
+			foreach (var item in eventItems)
+			{
+				stringBuilder.AppendLine($"Event: {item.Title} - Location: {item.Location} - Date: {item.Date.ToString("HH:mm - dd/MM/yyyy")}");
+			}
+			return stringBuilder.ToString().TrimEnd();
+		}
+	}
+}
